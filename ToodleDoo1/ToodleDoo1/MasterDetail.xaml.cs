c7b79422819bb58a12e7e2eaa6aa79c7816ae9c0
Joe Slack <Joe Slack>
2017-07-26 11:55:18 +0100

    Add project files.

---
+++ ToodleDoo1/ToodleDoo1/MasterDetail.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ToodleDoo1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MDPage : MasterDetailPage
+    {
+        public MDPage()
+        {
+            Label header = new Label
+            {
+                Text = "Menu",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                HorizontalOptions = LayoutOptions.Center
+            };
+
+            // Assemble an array of NamedColor objects.
+            string[] Names=
+                {
+                   "Option 1", "Option 2", "Option 3"
+                };
+
+            // Create ListView for the master page.
+            ListView listView = new ListView
+            {
+                ItemsSource = Names
+            };
+
+            // Create the master page with the ListView.
+            this.Master = new ContentPage
+            {
+                Title = header.Text,
+                Content = new StackLayout
+                {
+                    Children =
+                    {
+                        header,
+                        listView
+                    }
+                }
+            };
+
+            // Create the detail page using NamedColorPage and wrap it in a
+            // navigation page to provide a NavigationBar and Toggle button
+            this.Detail = new NavigationPage(new ToDoInput());
+
+            // For Windows Phone, provide a way to get back to the master page.
+            if (Device.OS == TargetPlatform.WinPhone)
+            {
+                (this.Detail as ContentPage).Content.GestureRecognizers.Add(
+                    new TapGestureRecognizer((view) =>
+                    {
+                        this.IsPresented = true;
+                    }));
+            }
+
+            // Define a selected handler for the ListView.
+            listView.ItemSelected += (sender, args) =>
+            {
+                // Set the BindingContext of the detail page.
+                this.Detail.BindingContext = args.SelectedItem;
+
+                // Show the detail page.
+                this.IsPresented = false;
+            };
+
+            // Initialize the ListView selection.
+            listView.SelectedItem = Names[0];
+
+
+        }
+    }
+}
